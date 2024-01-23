528d588ce08cf30f37006508241b1e4d15f69b25
brettclaus <brett.m.claus@gmail.com>
2024-01-22 19:05:22 -0800

    .

--- PerfectPlace.js
+++ PerfectPlace.js
@@ -205,7 +205,6 @@ function createMap() {
     let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
- 
 
     // Initializing the map
     map = L.map("map-id", {
